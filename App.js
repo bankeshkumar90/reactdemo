34ba2ed8c2ea5552217311b023d99573d5eba6f8
bankeshkumar90 <bankeshkumar90@gmail.com>
2022-01-24 07:10:53 +0530

    react first demo commit

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import { View, SafeAreaView, Text, Button} from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Linking } from 'react-native';
+
+const App = () =>{
+  return (
+    <View style={styles.body}>
+      <Text style = {styles.text}>
+        This is some Text
+        </Text>
+        <Button title='Click' onPress = {()=> {Linking.openURL('https://www.google.com')}}>
+        </Button>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    flex:1,
+    alignItems : 'center', 
+    justifyContent: 'center' ,
+    backgroundColor:'#0000ff',
+   },
+   text:{
+      color:'#ffffff',
+      fontSize:30,
+      fontStyle:'italic'
+   }
+});
+export default App;
